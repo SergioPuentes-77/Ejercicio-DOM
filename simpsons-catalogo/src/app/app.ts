ed67df31df805310d4fb864fbcd1f266dc9f3252
Sergio Puentes <sergiogonzalez30102005@gmail.com>
2026-09-04 23:18:44 -0500

    Agrega proyecto simpsons-catalogo

---
+++ simpsons-catalogo/src/app/app.ts
@@ -0,0 +1,11 @@
+// src/app/app.ts
+import { Component } from '@angular/core';
+import { CharacterListComponent } from './components/character-list/character-list.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CharacterListComponent],
+  template: `<app-character-list></app-character-list>`
+})
+export class App {}
